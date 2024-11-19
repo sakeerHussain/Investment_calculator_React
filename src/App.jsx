f62ea5f36ff4051481150a9f45cd832892eed48d
unknown <sakeerhussain@gmail.com>
2024-11-19 21:54:43 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annulaInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  console.log(userInput);
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput}/>
+    </>
+  );
 }
 
-export default App
+export default App;
